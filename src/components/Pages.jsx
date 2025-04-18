f2a6715532c94127a878afb0b190bc8b3f0c5d0c
Pranjal Snehi <pranjal.snehi@gmail.com>
2025-04-18 13:14:33 +0530

    Minor visual changes

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -5,7 +5,7 @@ function Pages() {
   return (
     <div className=" w-full overflow-x-auto bg-mainBg">
       <Tabs />
-      <section className="min-h-[85vh]  bg-mainBg">
+      <section className="min-h-[85vh] bg-mainBg mr-4 border border-tabBorder">
         <Outlet />
       </section>
     </div>
